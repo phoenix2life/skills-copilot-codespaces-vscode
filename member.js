0371d8102fd2ac0d83d360b2f6c883bde0730e1a
Ankush Borse <arb.blogger@gmail.com>
2023-07-22 00:27:30 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,35 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var memberText = document.getElementById("member-text");
+  var memberSkills = document.getElementById("member-skills");
+  var memberSkillsText = document.getElementById("member-skills-text");
+  var memberSkillsText2 = document.getElementById("member-skills-text2");
+  var memberSkillsText3 = document.getElementById("member-skills-text3");
+  var memberSkillsText4 = document.getElementById("member-skills-text4");
+  var memberSkillsText5 = document.getElementById("member-skills-text5");
+  var memberSkillsText6 = document.getElementById("member-skills-text6");
+  var memberSkillsText7 = document.getElementById("member-skills-text7");
+  var memberSkillsText8 = document.getElementById("member-skills-text8");
+  var memberSkillsText9 = document.getElementById("member-skills-text9");
+  var memberSkillsText10 = document.getElementById("member-skills-text10");
+  var memberSkillsText11 = document.getElementById("member-skills-text11");
+  var memberSkillsText12 = document.getElementById("member-skills-text12");
+  var memberSkillsText13 = document.getElementById("member-skills-text13");
+  var memberSkillsText14 = document.getElementById("member-skills-text14");
+  var memberSkillsText15 = document.getElementById("member-skills-text15");
+  var memberSkillsText16 = document.getElementById("member-skills-text16");
+  var memberSkillsText17 = document.getElementById("member-skills-text17");
+  var memberSkillsText18 = document.getElementById("member-skills-text18");
+  var memberSkillsText19 = document.getElementById("member-skills-text19");
+  var memberSkillsText20 = document.getElementById("member-skills-text20");
+  var memberSkillsText21 = document.getElementById("member-skills-text21");
+  var memberSkillsText22 = document.getElementById("member-skills-text22");
+  var memberSkillsText23 = document.getElementById("member-skills-text23");
+  var memberSkillsText24 = document.getElementById("member-skills-text24");
+  var memberSkillsText25 = document.getElementById("member-skills-text25");
+  var memberSkillsText26 = document.getElementById("member-skills-text26");
+  var memberSkillsText27 = document.getElementById("member-skills-text27");
+  var memberSkillsText28 = document.getElementById("member-skills-text28");
+  var memberSkillsText29 = document.getElementById("member-skills-text29");
+  var memberSkills 
+}
